9dc97c275bf1656d496756c5a9b093eed9696d3c
Nitesh Shrestha <nitesh.shresthax@gmail.com>
2022-12-08 15:49:33 +0545

    Add project files.

---
+++ JQueryValidationExample/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using JQueryValidationExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JQueryValidationExample.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Game game)
+        {
+            return Json(new { redirect = "/Game" });
+        }
+    }
+}
---
+++ JQueryValidationExample/Models/Game.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JQueryValidationExample.Models
+{
+    public class Game
+    {
+        [Required(ErrorMessage = "Game code is required.")]
+        [MinLength(4, ErrorMessage = "Game code must be at least 4 characters.")]
+        public string Code { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Game name is required.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Game description is required.")]
+        public string Description { get; set; } = string.Empty;
+        
+        [Required(ErrorMessage = "Game price is required.")]
+        [Range(0, 10000, ErrorMessage = "Game price should be between 0 to 10000")]
+        public decimal Price { get; set; }
+    }
+}
